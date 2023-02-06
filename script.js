7741218a9235afb3b945df0d9772a6fc28a0f0b6
Ben Hopkins <bjhops17@gmail.com>
2023-02-06 15:52:40 -0800

    added delete option

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 let image = document.querySelector("input")
 
 function newBlock(event){
-    document.getElementById("body").insertAdjacentHTML("afterend",`<img src="${event.target.result}" id="image1" data-set="${document.getElementsByTagName("img").length}"style="width: 100%"/>
-    <div id="checkboxContainer">
-    <input type="checkbox" id="xs">
-    <label for="xs">xs</label><br>
-    <input type="checkbox" id="s">
-    <label for="s">s</label><br>
-    <input type="checkbox" id="m">
-    <label for="m">m</label><br>
-    <input type="checkbox" id="l">
-    <label for="l">l</label><br>
-    <input type="checkbox" id="xl">
-    <label for="xl">xl</label><br>
-    <input type="checkbox" id="xxl">
-    <label for="xxl">xxl</label><br>
-</div>
-<br>`)
+  let card = `
+  
+  <div class="card">
+  <div class="deleteContainer">
+  <button class="delete">X</button>
+  </div>
+<img src="${event.target.result}" id="image1" data-set="${document.getElementsByTagName("img").length}"style="width: 100%"/>
+  <div id="checkboxContainer">
+  <input type="checkbox" id="xs">
+  <label for="xs">xs</label><br>
+  <input type="checkbox" id="s">
+  <label for="s">s</label><br>
+  <input type="checkbox" id="m">
+  <label for="m">m</label><br>
+  <input type="checkbox" id="l">
+  <label for="l">l</label><br>
+  <input type="checkbox" id="xl">
+  <label for="xl">xl</label><br>
+  <input type="checkbox" id="xxl">
+  <label for="xxl">xxl</label><br>
+</div></div>
+<br>`
+    document.getElementById("body").innerHTML += card
 
 }
 
@@ -31,7 +38,14 @@ function newBlock(event){
         newBlock(event)
   };
   reader.readAsDataURL(selectedFile);
-  document.body.getElementById("imageUpload").value=""
+ 
 
   })
+  let deleteButtons = document.getElementById("body").addEventListener("click", function(e){
+    if(e.target.getAttribute("class") === "delete"){
+      e.target.parentElement.parentElement.style.display = "none"
+    }
+   
+  })
+  
 
