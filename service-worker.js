381cceea7d2d02ce72c96a4d2c4eccb88af3a83f
Ben Hopkins <bjhops17@gmail.com>
2023-02-06 14:54:06 -0800

    added caching

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+const cacheName = "js13kPWA-v1";
+const appShellFiles = ["./index.html", "./script.js", "./style.css"]
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
@@ -14,4 +16,29 @@ if ('serviceWorker' in navigator) {
   this.addEventListener('fetch', function (event) {
     // This fetch function is required for the SW to be detected and is intentionally empty
     // For a more robust, real-world SW example see: https://developers.google.com/web/fundamentals/primers/service-workers
+    e.respondWith(
+        (async () => {
+          const r = await caches.match(e.request);
+          console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+          if (r) {
+            return r;
+          }
+          const response = await fetch(e.request);
+          const cache = await caches.open(cacheName);
+          console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+          cache.put(e.request, response.clone());
+          return response;
+        })()
+      );
+  });
+
+  self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        (async () => {
+          const cache = await caches.open(cacheName);
+          console.log("[Service Worker] Caching all: app shell and content");
+          await cache.addAll(appShellFiles);
+        })()
+      );
   });
